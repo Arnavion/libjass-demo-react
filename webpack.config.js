d57461fe580417eb71e875d7ac364dabf9a5c6d4
Arnavion <arnavion@gmail.com>
2016-02-06 23:11:40 -0800

    Fixed typo in loader test for JSX files.

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
 	output: { path: __dirname, filename: "index.js" },
 	module: {
 		loaders: [{
-			test: /.jsx?$/,
+			test: /\.jsx?$/,
 			loader: "babel-loader",
 			exclude: /node_modules/,
 			query: {
